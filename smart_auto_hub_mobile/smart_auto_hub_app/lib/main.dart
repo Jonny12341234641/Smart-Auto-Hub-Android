60093ff9eccddce970f7805fd6fc5c900749bf77
Kavindu <kavinduuni@gmail.com>
2026-03-10 15:52:06 +0530

    feat: Refactor app structure and enhance theme and splash screen design

--- smart_auto_hub_mobile/smart_auto_hub_app/lib/main.dart
+++ smart_auto_hub_mobile/smart_auto_hub_app/lib/main.dart
@@ -1,8 +1,22 @@
+// // Path: lib/main.dart
+// import 'package:flutter/material.dart';
+// import 'package:smart_auto_hub_app/app.dart';
+
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   runApp(const SmartAutoHubApp());
+// }
+
+//Gemini Code
+
 // Path: lib/main.dart
+
 import 'package:flutter/material.dart';
-import 'package:smart_auto_hub_app/app.dart';
+import 'app.dart';
 
 void main() {
+  // Ensure Flutter bindings are initialized
   WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const SmartAutoHubApp());
 }
